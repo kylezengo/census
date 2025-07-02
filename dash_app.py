cc6aeb562bc926f4555a1d89d92d649964a0a30b
kyle-SWSK <zengokp@gmail.com>
2025-07-02 17:46:24 -0400

    blah

---
+++ dash_app.py
@@ -0,0 +1,208 @@
+"""Create an interactive plot in a browser window"""
+
+import folium
+import geopandas as gpd
+import pandas as pd
+from dash import Dash, html, dcc, Input, Output
+
+MALE_COLOR = "Blues"
+FEMALE_COLOR = "Reds"
+MF_COLOR = "RdBu"
+INCOME_COLOR = "Greens"
+
+# Load files
+zcta_geom_raw = gpd.read_file("zcta_geom.shp") # needs zcta_df.cpg, zcta_df.shx, etc
+
+zcta_to_dma = pd.read_csv("zcta_to_dma.csv", dtype={'zcta':object})
+
+dma_polygons_raw = gpd.read_file('dma_polygons.geojson') # https://team.carto.com/u/andrew/tables/dma_master_polygons/public
+dma_polygons_raw['cartodb_id'] = dma_polygons_raw['cartodb_id'].astype(str)
+dma_polygons_raw['dma_code'] = dma_polygons_raw['dma_code'].astype(str)
+
+dma_polygon_map = pd.read_csv('dma_polygon_map.csv')
+
+c_zcta_dma = pd.read_csv("c_zcta_dma.csv", dtype={'zcta':object})
+
+c_dma = pd.read_csv("c_dma.csv")
+
+# Set up the geographic geometry files
+dma_polygons = dma_polygons_raw.merge(dma_polygon_map, left_on='dma_name', right_on='DMA Polygons')
+
+dma_geo = dma_polygons[['DMA', 'geometry']]
+dma_geo = dma_geo.set_index('DMA')
+
+dma_geo_json = dma_geo.to_json()
+
+zcta_geom = zcta_geom_raw.merge(zcta_to_dma[['zcta','dma']], how="left", left_on='ZCTA5CE20', right_on='zcta')
+
+# Create a DMA indexed version of the dataframes so we can lookup values
+c_dma_indexed = c_dma.set_index('dma')
+
+
+# Lists for user
+metric_cols = [col for col in c_dma.columns if col != 'dma']
+metric_cols.sort()
+
+dmas = c_dma['dma'].unique()
+# dmas.sort()
+
+# Set up Dash app
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Census Data Explorer", style={'fontFamily': 'Arial'}),
+
+    html.Div([
+        html.Label("Select Metrics", style={'fontFamily': 'Arial','fontWeight': 'bold'}),
+        dcc.Dropdown(
+            id='metric-selector',
+            options=metric_cols,
+            value=['Pop - Total'],
+            multi=True,
+            placeholder="Select metrics...",
+            style={'fontFamily': 'Arial'}
+        ),
+        html.Iframe(id='dma_map', width='100%', height='550')
+    ]),
+    html.Div([
+        html.Label("Select DMA", style={'fontFamily': 'Arial','fontWeight': 'bold'}),
+        dcc.Dropdown(
+            id='dma-selector',
+            options=dmas,
+            value='New York',
+            multi=False,
+            placeholder="Select DMA...",
+            style={'fontFamily': 'Arial'}
+        ),
+        html.Iframe(id='zcta_map', width='100%', height='550')
+    ])
+])
+
+def generate_dma_map(selected_metrics):
+    """
+    Build folium map with user selected metrics
+    """
+    m = folium.Map(tiles=None)
+
+    for i in selected_metrics:
+        if "Female" in i:
+            my_color = FEMALE_COLOR
+        elif "mf_ratio" in i:
+            my_color = MF_COLOR
+        elif "Income" in i:
+            my_color = INCOME_COLOR
+        else:
+            my_color = MALE_COLOR
+
+        my_chp = folium.Choropleth(
+            tiles="cartodb positron",
+            geo_data=dma_geo_json,
+            data=c_dma,
+            columns=['dma', i],
+            key_on="feature.id",
+            fill_opacity=0.7,
+            fill_color=my_color,
+            nan_fill_color="white",
+            nan_fill_opacity=0,
+            line_opacity=0.2,
+            line_weight=0.1,
+            legend_name=i,
+            highlight=True,
+            name=i,
+            overlay=False
+        ).add_to(m)
+
+        # Loop through the geojson object and add a new property (i) and assign a value from dataframe
+        for s in my_chp.geojson.data['features']:
+            if s['id'] in list(c_dma['dma']):
+                val = c_dma_indexed.loc[s['id'], i]
+            else:
+                val = 0
+            s['properties']['DMA'] = s['id']
+            s['properties'][i] = val
+        # add a tooltip/hover to the choropleth's geojson
+        folium.GeoJsonTooltip(['DMA',i]).add_to(my_chp.geojson)
+
+    folium.TileLayer(tiles='cartodb positron',control=False).add_to(m)
+    folium.LayerControl().add_to(m)
+    m.fit_bounds(m.get_bounds(), padding=(10, 10))
+    return m.get_root().render()
+
+def generate_zcta_map(selected_metrics, selected_dma):
+    """
+    Build folium map with user selected metrics
+    """
+    zcta_geom_select = zcta_geom[zcta_geom['dma']==selected_dma].reset_index()
+    zcta_geom_select = zcta_geom_select[['ZCTA5CE20', 'geometry']]
+    zcta_geom_select = zcta_geom_select.set_index('ZCTA5CE20')
+    zcta_json_select = zcta_geom_select.to_json()
+
+    c_zcta_dma_select = c_zcta_dma[c_zcta_dma['dma']==selected_dma].reset_index(drop=True)
+    c_zcta_dma_select = c_zcta_dma_select.rename(columns={'zcta': 'ZCTA5CE20'})
+
+    c_zcta_dma_select_indexed = c_zcta_dma_select.set_index('ZCTA5CE20')
+
+    m = folium.Map(tiles=None)
+
+    for i in selected_metrics:
+        if "Female" in i:
+            my_color = FEMALE_COLOR
+        elif "mf_ratio" in i:
+            my_color = MF_COLOR
+        elif "Income" in i:
+            my_color = INCOME_COLOR
+        else:
+            my_color = MALE_COLOR
+
+        my_chp = folium.Choropleth(
+            tiles="cartodb positron",
+            geo_data=zcta_json_select,
+            data=c_zcta_dma_select,
+            columns=['ZCTA5CE20', i],
+            key_on="feature.id",
+            fill_opacity=0.7,
+            fill_color=my_color,
+            nan_fill_color="white",
+            nan_fill_opacity=0,
+            line_opacity=0.2,
+            line_weight=0.1,
+            legend_name=i,
+            highlight=True,
+            name=i,
+            overlay=False
+        ).add_to(m)
+
+        # Loop through the geojson object and add a new property (i) and assign a value from dataframe
+        for s in my_chp.geojson.data['features']:
+            if s['id'] in list(c_zcta_dma_select['ZCTA5CE20']):
+                val = c_zcta_dma_select_indexed.loc[s['id'], i]
+            else:
+                val = 0
+            s['properties']['ZCTA'] = s['id']
+            s['properties'][i] = val
+        # add a tooltip/hover to the choropleth's geojson
+        folium.GeoJsonTooltip(['ZCTA',i]).add_to(my_chp.geojson)
+
+    folium.TileLayer(tiles='cartodb positron',control=False).add_to(m)
+    folium.LayerControl().add_to(m)
+    m.fit_bounds(m.get_bounds(), padding=(10, 10))
+    return m.get_root().render()
+
+# Callback to update the map
+@app.callback(
+    Output('dma_map', 'srcDoc'),
+    Input('metric-selector', 'value')
+)
+def update_dma_map(metrics):
+    return generate_dma_map(metrics)
+
+@app.callback(
+    Output("zcta_map", "srcDoc"),
+    Input('metric-selector', 'value'),
+    Input("dma-selector", "value")
+)
+def update_zcta_map(metrics, dma):
+    return generate_zcta_map(metrics, dma)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ download.py
@@ -0,0 +1,273 @@
+""" Download census data"""
+
+import os
+import requests
+
+import numpy as np
+import pandas as pd
+import pygris
+from dotenv import load_dotenv
+
+ACS_URL = "https://api.census.gov/data/2022/acs/acs5"
+
+# Authentication
+load_dotenv()
+census_api_key = os.getenv("census_api_key")
+
+
+####################################################################################################
+############################################ LOAD FILES ############################################
+####################################################################################################
+# ZIP to ZCTA source: https://udsmapper.org/zip-code-to-zcta-crosswalk/
+zip_to_zcta_raw = pd.read_excel(
+    "ZIPCodetoZCTACrosswalk2021UDS.xlsx",
+    dtype = {'ZIP_CODE':object,'ZCTA':object}
+)
+zip_to_dma_raw = pd.read_csv("zip_to_dma.csv", dtype = {'zip_code':object, 'dma_code':object})
+
+
+
+####################################################################################################
+########################################## DOWNLOAD DATA ###########################################
+####################################################################################################
+# Get the ZCTA cartographic boundary file with cb = True, seems to only work for 2020 right now
+zcta_geom = pygris.zctas(year=2020, cb = True)
+zcta_geom.to_file("zcta_geom.shp", index=False)
+
+
+# Get variable options
+response = requests.get("https://api.census.gov/data/2022/acs/acs5/variables.json", timeout=20)
+variables_json = response.json()
+
+variables = pd.DataFrame.from_dict(variables_json["variables"], orient="index")
+variables = variables.reset_index(names='variable')
+
+# Get data for variables
+variables_to_get = variables.loc[variables["group"] == "B01001",'variable']
+var_str = ",".join(variables_to_get) # convert to comma-separated string for API call
+
+params = {
+    "get": f"NAME,{var_str}",
+    "for": "zip code tabulation area:*",
+    "key": census_api_key
+}
+response = requests.get(ACS_URL, params=params, timeout=20)
+
+if response.status_code != 200:
+    print(f"Error {response.status_code}: {response.text}")
+else:
+    data = response.json()
+    data_df = pd.DataFrame(data[1:], columns=data[0])
+    data_df = data_df.drop(columns='NAME')
+
+# Calls are seperated because I can only request 50 variables at once
+more_variables_to_get = [
+    "B11012_001E", # N Census Households
+    "B19001_014E", # N Census Household Income 100-124
+    "B19001_015E", # N Census Household Income 125-149
+    "B19001_016E", # N Census Household Income 150-199
+    "B19001_017E", # N Census Household Income 200+
+    "B19049_003E"  # Median Census Household Income 25-44
+]
+var_str = ",".join(more_variables_to_get)
+
+params = {
+    "get": f"NAME,{var_str}",
+    "for": "zip code tabulation area:*",
+    "key": census_api_key
+}
+response = requests.get(ACS_URL, params=params, timeout=20)
+
+if response.status_code != 200:
+    print(f"Error {response.status_code}: {response.text}")
+else:
+    more_data = response.json()
+    more_data_df = pd.DataFrame(more_data[1:], columns=more_data[0])
+    more_data_df = more_data_df.drop(columns='NAME')
+
+
+
+####################################################################################################
+############################################# CLEAN UP #############################################
+####################################################################################################
+# Create zcta_to_dma
+zip_to_zcta = zip_to_zcta_raw.copy()
+zip_to_zcta.columns = zip_to_zcta.columns.str.lower()
+
+zip_to_dma = zip_to_dma_raw.rename(columns={'dma_description_clean':'dma'})
+zip_to_dma = zip_to_dma.drop(columns=['dma_description'])
+
+zip_to_zcta = zip_to_zcta.loc[~zip_to_zcta['state'].isin(["PR","VI"])]
+
+zip_to_zcta_dma = zip_to_zcta.merge(zip_to_dma, how='left', on='zip_code') # might want full outer?
+zip_to_zcta_dma = zip_to_zcta_dma.merge(
+    zip_to_dma.rename(columns={
+        'zip_code':'zip_code_zcta_join'
+        ,'dma_code':'dma_code_zcta_join'
+        ,'dma':'dma_zcta_join'
+    }),
+    how='left',
+    left_on='zcta',
+    right_on='zip_code_zcta_join'
+)
+zip_to_zcta_dma['dma_code'] = zip_to_zcta_dma['dma_code'].fillna(zip_to_zcta_dma['dma_code_zcta_join'])
+zip_to_zcta_dma['dma'] = zip_to_zcta_dma['dma'].fillna(zip_to_zcta_dma['dma_zcta_join'])
+zip_to_zcta_dma = zip_to_zcta_dma.drop(columns=['zip_code_zcta_join','dma_code_zcta_join','dma_zcta_join'])
+
+zcta_to_dma = zip_to_zcta_dma[['zcta','dma']].drop_duplicates()
+
+# Clean up census data
+c_zcta = data_df.merge(more_data_df, how='outer', on='zip code tabulation area') # maybe can get away with inner join?
+
+# Convert numbers from string to integer
+metrics = list(variables_to_get) + more_variables_to_get
+
+c_zcta[metrics] = c_zcta[metrics].astype(float)
+
+# Rename columns for clarity
+c_zcta = c_zcta.rename(columns={
+    'zip code tabulation area': 'zcta'
+    ,'B01001_001E': 'Pop - Total'
+    ,'B01001_002E': 'Pop - Male'
+    ,'B01001_003E': 'Pop - Male Under 5 years'
+    ,'B01001_004E': 'Pop - Male 5 to 9 years'
+    ,'B01001_005E': 'Pop - Male 10 to 14 years'
+    ,'B01001_006E': 'Pop - Male 15 to 17 years'
+    ,'B01001_007E': 'Pop - Male 18 and 19 years'
+    ,'B01001_008E': 'Pop - Male 20 years'
+    ,'B01001_009E': 'Pop - Male 21 years'
+    ,'B01001_010E': 'Pop - Male 22 to 24 years'
+    ,'B01001_011E': 'Pop - Male 25 to 29 years'
+    ,'B01001_012E': 'Pop - Male 30 to 34 years'
+    ,'B01001_013E': 'Pop - Male 35 to 39 years'
+    ,'B01001_014E': 'Pop - Male 40 to 44 years'
+    ,'B01001_015E': 'Pop - Male 45 to 49 years'
+    ,'B01001_016E': 'Pop - Male 50 to 54 years'
+    ,'B01001_017E': 'Pop - Male 55 to 59 years'
+    ,'B01001_018E': 'Pop - Male 60 and 61 years'
+    ,'B01001_019E': 'Pop - Male 62 to 64 years'
+    ,'B01001_020E': 'Pop - Male 65 and 66 years'
+    ,'B01001_021E': 'Pop - Male 67 to 69 years'
+    ,'B01001_022E': 'Pop - Male 70 to 74 years'
+    ,'B01001_023E': 'Pop - Male 75 to 79 years'
+    ,'B01001_024E': 'Pop - Male 80 to 84 years'
+    ,'B01001_025E': 'Pop - Male 85 years and over'
+    ,'B01001_026E': 'Pop - Female'
+    ,'B01001_027E': 'Pop - Female Under 5 years'
+    ,'B01001_028E': 'Pop - Female 5 to 9 years'
+    ,'B01001_029E': 'Pop - Female 10 to 14 years'
+    ,'B01001_030E': 'Pop - Female 15 to 17 years'
+    ,'B01001_031E': 'Pop - Female 18 and 19 years'
+    ,'B01001_032E': 'Pop - Female 20 years'
+    ,'B01001_033E': 'Pop - Female 21 years'
+    ,'B01001_034E': 'Pop - Female 22 to 24 years'
+    ,'B01001_035E': 'Pop - Female 25 to 29 years'
+    ,'B01001_036E': 'Pop - Female 30 to 34 years'
+    ,'B01001_037E': 'Pop - Female 35 to 39 years'
+    ,'B01001_038E': 'Pop - Female 40 to 44 years'
+    ,'B01001_039E': 'Pop - Female 45 to 49 years'
+    ,'B01001_040E': 'Pop - Female 50 to 54 years'
+    ,'B01001_041E': 'Pop - Female 55 to 59 years'
+    ,'B01001_042E': 'Pop - Female 60 and 61 years'
+    ,'B01001_043E': 'Pop - Female 62 to 64 years'
+    ,'B01001_044E': 'Pop - Female 65 and 66 years'
+    ,'B01001_045E': 'Pop - Female 67 to 69 years'
+    ,'B01001_046E': 'Pop - Female 70 to 74 years'
+    ,'B01001_047E': 'Pop - Female 75 to 79 years'
+    ,'B01001_048E': 'Pop - Female 80 to 84 years'
+    ,'B01001_049E': 'Pop - Female 85 years and over'
+    ,'B11012_001E': 'N Census Households'
+    ,'B19001_014E': 'N Census Household Income 100-124'
+    ,'B19001_015E': 'N Census Household Income 125-149'
+    ,'B19001_016E': 'N Census Household Income 150-199'
+    ,'B19001_017E': 'N Census Household Income 200+'
+    ,'B19049_003E': 'Median Census Household Income 25-44'
+})
+
+# Aggregate decades
+c_zcta['Pop - Male Under 10 years'] = c_zcta['Pop - Male Under 5 years'] + c_zcta['Pop - Male 5 to 9 years']
+c_zcta['Pop - Male 10 to 19 years'] = (c_zcta['Pop - Male 10 to 14 years'] +
+                                       c_zcta['Pop - Male 15 to 17 years'] +
+                                       c_zcta['Pop - Male 18 and 19 years'])
+c_zcta['Pop - Male 20 to 29 years'] = (c_zcta['Pop - Male 20 years'] +
+                                       c_zcta['Pop - Male 21 years'] +
+                                       c_zcta['Pop - Male 22 to 24 years'] +
+                                       c_zcta['Pop - Male 25 to 29 years'])
+c_zcta['Pop - Male 30 to 39 years'] = c_zcta['Pop - Male 30 to 34 years'] + c_zcta['Pop - Male 35 to 39 years']
+c_zcta['Pop - Male 40 to 49 years'] = c_zcta['Pop - Male 40 to 44 years'] + c_zcta['Pop - Male 45 to 49 years']
+c_zcta['Pop - Male 50 to 59 years'] = c_zcta['Pop - Male 50 to 54 years'] + c_zcta['Pop - Male 55 to 59 years']
+c_zcta['Pop - Male 60 to 69 years'] = (c_zcta['Pop - Male 60 and 61 years'] +
+                                       c_zcta['Pop - Male 62 to 64 years'] +
+                                       c_zcta['Pop - Male 65 and 66 years'] +
+                                       c_zcta['Pop - Male 67 to 69 years'])
+c_zcta['Pop - Male 70 to 79 years'] = c_zcta['Pop - Male 70 to 74 years'] + c_zcta['Pop - Male 75 to 79 years']
+c_zcta['Pop - Male 80 years and over'] = c_zcta['Pop - Male 80 to 84 years'] + c_zcta['Pop - Male 85 years and over']
+
+c_zcta['Pop - Female Under 10 years'] = c_zcta['Pop - Female Under 5 years'] + c_zcta['Pop - Female 5 to 9 years']
+c_zcta['Pop - Female 10 to 19 years'] = (c_zcta['Pop - Female 10 to 14 years'] +
+                                         c_zcta['Pop - Female 15 to 17 years'] +
+                                         c_zcta['Pop - Female 18 and 19 years'])
+c_zcta['Pop - Female 20 to 29 years'] = (c_zcta['Pop - Female 20 years'] +
+                                         c_zcta['Pop - Female 21 years'] +
+                                         c_zcta['Pop - Female 22 to 24 years'] +
+                                         c_zcta['Pop - Female 25 to 29 years'])
+c_zcta['Pop - Female 30 to 39 years'] = c_zcta['Pop - Female 30 to 34 years'] + c_zcta['Pop - Female 35 to 39 years']
+c_zcta['Pop - Female 40 to 49 years'] = c_zcta['Pop - Female 40 to 44 years'] + c_zcta['Pop - Female 45 to 49 years']
+c_zcta['Pop - Female 50 to 59 years'] = c_zcta['Pop - Female 50 to 54 years'] + c_zcta['Pop - Female 55 to 59 years']
+c_zcta['Pop - Female 60 to 69 years'] = (c_zcta['Pop - Female 60 and 61 years'] +
+                                         c_zcta['Pop - Female 62 to 64 years'] +
+                                         c_zcta['Pop - Female 65 and 66 years'] +
+                                         c_zcta['Pop - Female 67 to 69 years'])
+c_zcta['Pop - Female 70 to 79 years'] = c_zcta['Pop - Female 70 to 74 years'] + c_zcta['Pop - Female 75 to 79 years']
+c_zcta['Pop - Female 80 years and over'] = c_zcta['Pop - Female 80 to 84 years'] + c_zcta['Pop - Female 85 years and over']
+
+# Sex ratios
+c_zcta['mf_ratio'] = c_zcta['Pop - Male'] / c_zcta['Pop - Female']
+c_zcta['mf_ratio_Under 10 years'] = c_zcta['Pop - Male Under 10 years'] / c_zcta['Pop - Female Under 10 years']
+c_zcta['mf_ratio_10 to 19 years'] = c_zcta['Pop - Male 10 to 19 years'] / c_zcta['Pop - Female 10 to 19 years']
+c_zcta['mf_ratio_20 to 29 years'] = c_zcta['Pop - Male 20 to 29 years'] / c_zcta['Pop - Female 20 to 29 years']
+c_zcta['mf_ratio_30 to 39 years'] = c_zcta['Pop - Male 30 to 39 years'] / c_zcta['Pop - Female 30 to 39 years']
+c_zcta['mf_ratio_40 to 49 years'] = c_zcta['Pop - Male 40 to 49 years'] / c_zcta['Pop - Female 40 to 49 years']
+c_zcta['mf_ratio_50 to 59 years'] = c_zcta['Pop - Male 50 to 59 years'] / c_zcta['Pop - Female 50 to 59 years']
+c_zcta['mf_ratio_60 to 69 years'] = c_zcta['Pop - Male 60 to 69 years'] / c_zcta['Pop - Female 60 to 69 years']
+c_zcta['mf_ratio_70 to 79 years'] = c_zcta['Pop - Male 70 to 79 years'] / c_zcta['Pop - Female 70 to 79 years']
+c_zcta['mf_ratio_80 years and over'] = c_zcta['Pop - Male 80 years and over'] / c_zcta['Pop - Female 80 years and over']
+
+# Income ratios
+c_zcta['Household Income 200+_ratio'] = c_zcta['N Census Household Income 200+'] / c_zcta['N Census Households']
+
+c_zcta = c_zcta.replace([np.inf, -np.inf], np.nan)
+
+######################################################
+# DMA Stuff ##########################################
+c_zcta_dma = c_zcta.merge(zcta_to_dma, how='left', on='zcta')
+
+c_dma = c_zcta_dma.groupby('dma', as_index=False, dropna=False).sum(numeric_only=True)
+c_dma = c_dma.drop(columns='Median Census Household Income 25-44') # can do weighted avg or something instead
+
+pop_cols = [col for col in c_dma.columns if "Pop - " in col]
+c_dma[pop_cols] = c_dma[pop_cols].astype(float) # need float for map
+
+# Sex ratios
+c_dma['mf_ratio'] = c_dma['Pop - Male'] / c_dma['Pop - Female']
+c_dma['mf_ratio_Under 10 years'] = c_dma['Pop - Male Under 10 years'] / c_dma['Pop - Female Under 10 years']
+c_dma['mf_ratio_10 to 19 years'] = c_dma['Pop - Male 10 to 19 years'] / c_dma['Pop - Female 10 to 19 years']
+c_dma['mf_ratio_20 to 29 years'] = c_dma['Pop - Male 20 to 29 years'] / c_dma['Pop - Female 20 to 29 years']
+c_dma['mf_ratio_30 to 39 years'] = c_dma['Pop - Male 30 to 39 years'] / c_dma['Pop - Female 30 to 39 years']
+c_dma['mf_ratio_40 to 49 years'] = c_dma['Pop - Male 40 to 49 years'] / c_dma['Pop - Female 40 to 49 years']
+c_dma['mf_ratio_50 to 59 years'] = c_dma['Pop - Male 50 to 59 years'] / c_dma['Pop - Female 50 to 59 years']
+c_dma['mf_ratio_60 to 69 years'] = c_dma['Pop - Male 60 to 69 years'] / c_dma['Pop - Female 60 to 69 years']
+c_dma['mf_ratio_70 to 79 years'] = c_dma['Pop - Male 70 to 79 years'] / c_dma['Pop - Female 70 to 79 years']
+c_dma['mf_ratio_80 years and over'] = c_dma['Pop - Male 80 years and over'] / c_dma['Pop - Female 80 years and over']
+
+# Income ratios
+c_dma['Household Income 200+_ratio'] = c_dma['N Census Household Income 200+'] / c_dma['N Census Households']
+
+
+####################################################################################################
+############################################### SAVE ###############################################
+####################################################################################################
+ # Save csv (mostly static data so just overwrite)
+zcta_to_dma.to_csv("zcta_to_dma.csv", index=False)
+c_zcta_dma.to_csv("c_zcta_dma.csv", index=False)
+c_dma.to_csv("c_dma.csv", index=False)
